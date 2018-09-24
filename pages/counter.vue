d176364f66a2bdcd3009739c58fea1a0253ed4f2
Infant Frontender <newbornfrontender@gmail.com>
2018-09-24 20:27:23 +0300

    Добавил страницы

---
+++ pages/counter.vue
@@ -0,0 +1,42 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+
+    <div>
+      <p>Счетчик: {{ counter }}</p>
+
+      <button v-on:click="increment">+</button>
+      <button v-on:click="decrement">-</button>
+    </div>
+  </section>
+</template>
+
+<script>
+  import { mapState, mapMutations } from 'vuex';
+
+  export default {
+    name: 'counter',
+
+    data: () => ({
+      title: 'Счетчик',
+    }),
+
+    computed: mapState({
+      counter: state => state.counter.counter,
+    }),
+
+    methods: mapMutations({
+      increment: 'counter/increment',
+      decrement: 'counter/decrement',
+    }),
+
+    head () { return {
+      title: this.title,
+      meta: [{
+        hid: 'counter-description',
+        name: 'description',
+        content: 'Описание страницы счетчика',
+      }],
+    }},
+  };
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <section>
+    <h1>{{ title }}</h1>
+
+    <div>
+      <p>{{ counter }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'index',
+
+    data: () => ({
+      title: 'Главная',
+    }),
+
+    computed: mapState({
+      counter: state => state.counter.counter,
+    }),
+
+    head () { return {
+      title: this.title,
+      meta: [{
+        hid: 'index-description',
+        name: 'description',
+        content: 'Описание главной страницы',
+      }],
+    }},
+  };
+</script>
