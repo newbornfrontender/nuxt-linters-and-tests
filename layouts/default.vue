a37c1c1259054b58ecbabb9333e5bc11013183d8
Infant Frontender <newbornfrontender@gmail.com>
2018-09-24 20:27:53 +0300

    Добавил шаблоны

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <section>
+    <main-page-header />
+
+    <main-page-content />
+  </section>
+</template>
+
+<script>
+  import MainPageHeader from '~/components/MainPageHeader';
+  import MainPageContent from '~/components/MainPageContent';
+
+  export default {
+    name: 'default',
+
+    components: {
+      MainPageHeader,
+      MainPageContent,
+    },
+  };
+</script>
---
+++ layouts/error.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+    <h1 v-if="checkStatusCode(404)">{{ title }}</h1>
+    <h1 v-else>Ошибка в коде</h1>
+
+    <nuxt-link to="/">Перейти на главную страницу</nuxt-link>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: 'error',
+
+    props: {
+      error: {
+        type: Object,
+        default: () => {},
+      },
+    },
+
+    data: () => ({
+      title: 'Страница не найдена',
+    }),
+
+    methods: {
+      checkStatusCode (num) {
+        return this.error.statusCode === num;
+      },
+    },
+
+    head () { return {
+      title: this.title,
+      meta: [{
+        hid: 'error-description',
+        name: 'description',
+        content: 'Описание страницы с ошибкой',
+      }],
+    }},
+  };
+</script>
