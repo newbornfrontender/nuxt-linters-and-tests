019801327a87f031dc154cf772ef00097fe2e695
Infant Frontender <newbornfrontender@gmail.com>
2018-09-24 20:28:12 +0300

    Добавил компоненты

---
+++ components/MainPageHeader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav>
+    <ul>
+      <li>
+        <nuxt-link to="/">Главная</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link to="/counter">Счетчик</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+  export default {
+    name: 'MainPageHeader',
+  };
+</script>
